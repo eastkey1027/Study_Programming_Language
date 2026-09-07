9d8c17d88f96a36cb65feb8e5428943fd49f3f33
eastkey1027 <eastkey1027@naver.com>
2026-09-07 10:39:10 +0900

    data_type

---
+++ Python/NadoCoding_OneMinute/data_type.py
@@ -0,0 +1,12 @@
+# 숫자 자료형
+print(1) # 정수
+print(3.14) # 실수
+
+# 문자 자료형
+print('hello world') # 문자열
+print("안녕하세요") # 문자열
+print('10') # 문자열
+
+# 불리안 자료형, 반드시 첫글자는 대문자
+print(True) # 참
+print(False) # 거짓
